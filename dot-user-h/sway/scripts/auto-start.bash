e9f4c935196fe728bc2c13bcfb94444cd991a299
Your Name <you@example.com>
2025-11-13 12:40:45 +0000

    debpackage dot-user-h/sway/scripts/auto-start.bash

--- dot-user-h/sway/scripts/auto-start.bash
+++ dot-user-h/sway/scripts/auto-start.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
-swaymsg workspace 1 && swaymsg exec 'xfce4-terminal -x notes.bash'
+swaymsg workspace 1 && 
+swaymsg exec 'xfce4-terminal -x notes.bash'
+swaymsg exec "xfce4-terminal -e 'nvim /home/pub/警告(warning).md'"
 sleep 2
 
 swaymsg workspace 2 && swaymsg exec firefox-dev
@@ -7,4 +9,6 @@ sleep 2
 
 swaymsg workspace 3 && swaymsg exec firefox-esr
 sleep 2
+
+swaymsg workspace 1
 
